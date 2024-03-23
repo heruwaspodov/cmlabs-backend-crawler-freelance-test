6789fc0f752522bb6850c986ee06450d654e1806
Dwi H. Waspodov <wspdv@WSPDV.local>
2024-03-23 11:16:46 +0700

    changed: method HEAD to GET, some link not opened while using HEAD

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -1,6 +1,6 @@
 export const isLinkValid = async (url: string): Promise<boolean> => {
     try {
-        const response = await fetch(url, { method: 'HEAD' });
+        const response = await fetch(url, { method: 'GET' });
         return response.ok
     } catch {
         return false
